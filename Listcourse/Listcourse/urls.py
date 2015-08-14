0aad15bacb4b4d566c6a0ed4d0ed55dbf56c9a01
Alexis <ptpf.as@gmail.com>
2015-08-14 22:24:49 +0200

    initial url

--- Listcourse/Listcourse/urls.py
+++ Listcourse/Listcourse/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accueil/$', 'ingredient.views.home', name="accueil"),
+    url(r'^liste/$','ingredient.views.listView'),
+    url(r'^login/$','member.views.loginView', name='login'),
+    url(r'^logout/$', 'member.views.logoutView', name="logout"),
+    url(r'^addProductToList/$','ingredient.views.addProductToList', name='addProductToList'),
+    url(r'^createList/$','ingredient.views.createList', name='createList'),
+    url(r'^myLists/$','ingredient.views.myListsView', name='myLists'),
+
+
 ]
